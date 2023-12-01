ff17a2eebeed7ac127476895865c1519d83a3146
Holger Dobosz <hdobosz@freenet.de>
2023-12-01 16:13:12 +0100

    changes in the url

--- Users/urls.py
+++ Users/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('dj-rest-auth/registration/account-confirm-email/<str:key>/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     path('auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('dj-rest-auth/login/', CustomLoginView.as_view(), name='custom-login'),
+    path('dj-rest-auth/login_the_right/', CustomLoginView.as_view(), name='custom-login'),
     
 ]
