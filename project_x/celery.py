2b09aa008a8b61707f4bc7f51e9bc98ee657e6c9
Holger Dobosz <hdobosz@freenet.de>
2023-12-22 09:45:36 +0100

    problemsolving in celery

--- project_x/celery.py
+++ project_x/celery.py
@@ -3,15 +3,11 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 from django.conf import settings
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_x.settings')
-
 app = Celery('Data')
 app.conf.timezone = 'Europe/Berlin'
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
 app.conf.beat_schedule = {
     'fetch-forex-data': {
         'task': 'Data.tasks.fetch_forex_data',
@@ -28,7 +24,7 @@ app.conf.beat_schedule = {
     'fetch-forex-data3': {
         'task': 'Data.tasks.fetch_forex_data3',
         'schedule': crontab(minute='*'),
-    },   
+    },
     'fetch-forex-data4': {
         'task': 'Data.tasks.fetch_forex_data4',
         'schedule': crontab(minute='*'),
@@ -44,15 +40,19 @@ app.conf.beat_schedule = {
     'fetch-stock-data': {
         'task': 'Data.tasks.fetch_stock_data',
         'schedule': crontab(minute='*'),
+    },
     'fetch-stock-data1': {
         'task': 'Data.tasks.fetch_stock_data1',
         'schedule': crontab(minute='*'),
+    },
     'fetch-stock-data2': {
         'task': 'Data.tasks.fetch_stock_data2',
         'schedule': crontab(minute='*'),
+    },
     'fetch-stock-data3': {
         'task': 'Data.tasks.fetch_stock_data3',
         'schedule': crontab(minute='*'),
+    },
     'fetch-stock-data4': {
         'task': 'Data.tasks.fetch_stock_data4',
         'schedule': crontab(minute='*'),
@@ -106,7 +106,6 @@ app.conf.beat_schedule = {
         'schedule': crontab(minute='*'),
     },
  }
-
 app.conf.broker_connection_retry_on_startup = True
 app.conf.beat_scheduler = 'celery.beat.PersistentScheduler'
 app.conf.result_backend = 'rpc://'
