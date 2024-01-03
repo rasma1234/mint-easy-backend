8f09fa1ab55cac539d2fe912f483b30aeeb0e33d
eltonvansoylent <eltonvansoylent@gmail.com>
2024-01-03 14:56:58 +0100

    password reset email custom template + clean up

--- Users/apps.py
+++ Users/apps.py
@@ -2,9 +2,5 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Users'
-
-    def ready(self): #new
-        import Users.signals  
-        
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Users"
