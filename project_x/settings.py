d53a8699a7549679c4551767c0da33dd7fcb02c5
Holger Dobosz <hdobosz@freenet.de>
2024-02-13 14:57:28 +0100

    default SecretKey

--- project_x/settings.py
+++ project_x/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRETKEY")
+SECRET_KEY = config("SECRETKEY", default='2J!l9#lP1*qWl&m6Nw#Mv!5E~3FxR?fQ$oN%uHn3QoR$+KdD')
+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
